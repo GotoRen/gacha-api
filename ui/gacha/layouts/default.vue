9c31ae9bc3f716096517097efd029084b3ba8d1e
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2021-04-09 19:33:13 +0900

    uiの修正,storeの導入

--- ui/gacha/layouts/default.vue
+++ ui/gacha/layouts/default.vue
@@ -5,25 +5,30 @@
       app
     >
       <v-toolbar-title v-text="title" />
-      {{ id }}
       <v-spacer />
+      <v-btn
+        v-if=!$store.state.login
+        color="primary"
+        dark
+        @click.stop="loginDialog = true"
+      >
+        Login
+      </v-btn>
+      <v-btn
+        v-if=$store.state.login
+        color="primary"
+        dark
+        @click.stop="userDialog = true"
+      >
+        {{ $store.state.username }}
+      </v-btn>
       <v-dialog
       v-model="loginDialog"
       max-width="600px"
       >
-        <template v-if=!login v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            ログイン
-          </v-btn>
-        </template>
         <v-card>
           <v-card-title>
-            <span class="headline">login form</span>
+            <span class="headline">LoginForm</span>
           </v-card-title>
           <v-card-text>
             <v-container>
@@ -51,15 +56,15 @@
               text
               @click="loginDialog = false;signupDialog = true"
             >
-              サインアップ
+              SignUp
             </v-btn>
             <v-spacer></v-spacer>
             <v-btn
               color="blue darken-1"
               text
-              @click="getTokenRequest();loginDialog = false"
+              @click="loginRequest();loginDialog = false"
             >
-              ログイン
+              Login
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -70,7 +75,7 @@
       >
         <v-card>
           <v-card-title>
-            <span class="headline">signup form</span>
+            <span class="headline">SignUpForm</span>
           </v-card-title>
           <v-card-text>
             <v-container>
@@ -91,15 +96,55 @@
               text
               @click="loginDialog = true;signupDialog = false"
             >
-              ログイン
+              Login
             </v-btn>
             <v-spacer></v-spacer>
             <v-btn
               color="blue darken-1"
               text
-              @click="signupDialog = false;"
+              @click="userCreateRequest();signupDialog = false;"
             >
-              サインアップ
+              SignUp
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog
+      v-model="userDialog"
+      max-width="600px"
+      >
+        <v-card>
+          <v-card-title>
+            <span class="headline">{{ $store.state.username }}</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field
+                    label="username*"
+                    v-model=username
+                    required
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="userDialog = false;logout();"
+            >
+              Logout
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="userDialog = false;updateUserName();"
+            >
+              Rename
             </v-btn>
           </v-card-actions>
         </v-card>
@@ -124,18 +169,13 @@ export default {
   data () {
     return {
       id: 0,
-      username: "",
-      login: false,
-      user: "ログイン",
+      username: '',
+      token: '',
       fixed: false,
       title: 'gacha-api',
-      token: '',
       loginDialog: false,
       signupDialog: false,
-      loginjson:{
-        'id':this.id,
-        'name':this.username
-      }
+      userDialog: false
     }
   },
   methods: {
@@ -146,11 +186,11 @@ export default {
           'Content-Type':'application/json'
         }
       }).then(response => {
-        this.user = response;
-        console.log(this.user);
+        this.username = response;
+        console.log(this.username);
       });
     },
-    async getTokenRequest() {
+    async loginRequest() {
       await this.$axios.$post('http://localhost:8080/user/login',{
         'id':Number(this.id),
         'name':this.username
@@ -160,20 +200,47 @@ export default {
         }
       }).then(response => {
         this.token = response;
+        this.$store.commit('changeLogin', true);
+        this.$store.commit('changeUserData', {
+          id: this.id,
+          username: this.username,
+          token: this.token
+        });
         console.log(this.token);
       });
     },
-    async userCreateRequest() {
-      await this.$axios.$post('http://localhost:8080/user/create',{
-        headers: {
-          'Content-Type':'application/json'
-        },
-        body: {
-          'name':this.username
-        }
-      }).then(response => {
-        this.token = response;
-        console.log(this.token);
+    // async userCreateRequest() {
+    //   await this.$axios.$post('http://localhost:8080/user/create',{
+    //     headers: {
+    //       'Content-Type':'application/json'
+    //     },
+    //     body: {
+    //       'name':this.username
+    //     }
+    //   }).then(response => {
+    //     this.token = response.token;
+    //     this.id = response.id;
+    //     this.$store.commit('changeLogin', true);
+    //     this.$store.commit('changeUserData', {
+    //       id: this.id,
+    //       username: this.username,
+    //       token: this.token
+    //     });
+    //     console.log(this.token);
+    //   });
+    // },
+    userCreateRequest() {
+      console.log('apiが未完成です')
+    },
+    logout() {
+      this.id = 0;
+      this.username = '';
+      this.token = '';
+      this.$store.commit('changeLogin', false);
+      this.$store.commit('changeUserData', {
+        id: this.id,
+        username: this.username,
+        token: this.token
       });
     }
   }
