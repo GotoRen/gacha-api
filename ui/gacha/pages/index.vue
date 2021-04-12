2216e6178ed07ffbd645e77da17cc4bb11de2c79
KobayashiFumiaki <k18032kk@aitech.ac.jp>
2021-04-12 20:14:19 +0900

    uiの修正

--- ui/gacha/pages/index.vue
+++ ui/gacha/pages/index.vue
@@ -3,7 +3,6 @@
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8" md="6">
         <p>{{ result }}</p>
-         <v-text-field label="ガチャ回数" outlined v-model.number="num"/>
          <v-subheader>ガチャ回数</v-subheader>
          <v-slider
             v-model.number="num"
@@ -13,9 +12,8 @@
           <template v-slot:append>
             <v-text-field
               v-model.number="num"
-              class="mt-0 pt-0"
               type="number"
-              style="width: 60px"
+              outlined
             ></v-text-field>
           </template>
         </v-slider>
